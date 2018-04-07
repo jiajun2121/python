76fe476186af452ee95a7e2f670204f7c3cbd15a
jiajun2121 <32513558+jiajun2121@users.noreply.github.com>
2018-04-07 12:39:01 +0800

    matplotlib 练习

--- plot/main.py
+++ plot/main.py
@@ -1,39 +1,39 @@
 
 
-
-import sys
-import math
 import codecs
+import math
 import string
+import sys
+import time as t
+
+import matplotlib.pyplot as plt
+import numpy as np
 
 
+def money_580(calltime=0):
+    costpersecond=0.19
+    call_long=150
+    base_cost=18
+    cost = (calltime*(calltime>call_long)+call_long*(calltime<=call_long) )*costpersecond+base_cost
+    return cost
 
-fil = codecs.open("SinaWeibo_FollowNumber.txt",'r','utf-8')
-
-infm = fil.readline()
-infm = fil.readline()
-
-fensitemp = infm[20:len(infm)]
-fensitemp = fensitemp.rstrip()
-fensitemp = fensitemp.lstrip()	
-
-
-
-
-
-
+def money_38(calltime=0):
+    costpersecond=0.19
+    call_long=100
+    base_cost=38
+    cost = (calltime*(calltime>call_long)+call_long*(calltime<=call_long) )*costpersecond+base_cost
+    return cost
 
 
-while (infm != ""):
-	date = infm[:10]
-	tim = infm[11:19]
-	fensitemp = infm[20:len(infm)]
-	fensitemp = fensitemp.rstrip()
-	fensitemp = fensitemp.lstrip()	
-	infm = fil.readline()
+t = np.arange(350)
+x = money_38(t)
+y = money_580(t)
 
+p1 = plt.plot(t,x)
+p2 = plt.plot(t,y)
+p3 = plt.plot(t,x-y)
 
-
-fil.close()
-
-
+#plt.text(0.1,0.5, r"$\zeta(s) = \sum_{n=0}^{\infty}{\frac{1}{n^s}} $",fontsize=50)
+plt.legend([p1,p2,p3],['38','580','diff'],loc='best')
+plt.grid(True,linestyle='-',color=[0.9,0.9,0.9],linewidth=1)
+plt.show()
