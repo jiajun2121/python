95d9b9b122c6507cb08cdb8860724d515555d578
jiajun2121 <32513558+jiajun2121@users.noreply.github.com>
2018-04-07 12:30:22 +0800

    校园网

---
+++ study/school校园网/sn.py
@@ -0,0 +1,24 @@
+user = '20140501055'
+pswd = '123456'
+
+
+
+login_page_url = 'http://172.17.255.20/eportal/userV2.do?method=toLogin#'
+validcode_url = 'http://172.17.255.20/eportal/validcode'
+
+def get_response(url):
+    pass
+
+
+
+def get_login_page(response):
+    pass
+
+def get_validcode_pic(response):
+    pass
+
+def ocr_code(file):
+    pass
+
+
+def login()
